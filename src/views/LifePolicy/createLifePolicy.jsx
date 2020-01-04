279a631f6742589b90ef64168dafb2ef271cf0df
Tushar Narula <tushar.narula@gmail.com>
2020-01-04 22:40:29 +0530

    basic ui life policy done

--- src/views/LifePolicy/createLifePolicy.jsx
+++ src/views/LifePolicy/createLifePolicy.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 // @material-ui/core
+import styles from "./lifePolicyStyles";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Paper,
@@ -7,7 +8,9 @@ import {
   InputLabel,
   Select,
   MenuItem,
-  Button
+  Button,
+  Typography,
+  FormControl
 } from "@material-ui/core";
 import {
   MuiPickersUtilsProvider,
@@ -15,58 +18,102 @@ import {
 } from "@material-ui/pickers";
 import Moment from "@date-io/moment";
 
+const useStyles = makeStyles(styles);
+
 export default function CreateLifePolicy() {
+  const classes = useStyles();
   return (
     <>
-      <Paper className="outerPaper">
-        <TextField className="nameInput" label="Name of Policy Holder" />
-
-        <MuiPickersUtilsProvider utils={Moment}>
-          <KeyboardDatePicker
-            margin="normal"
-            className="datePicker"
-            id="date-picker-dialog"
-            label="Date of Commencement"
-            format="MM/dd/yyyy"
-            //value={selectedDate}
-            //onChange={handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change date"
-            }}
+      <Paper className={classes.outerPaper}>
+        <div className={classes.elementDiv}>
+          <Typography variant="h5">
+            Create Life Insurance Policy Record
+          </Typography>
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField
+            className={classes.nameInput}
+            label="Name of Policy Holder"
           />
-        </MuiPickersUtilsProvider>
-        <TextField label="Plan/ Term" className="planInput" />
-        <InputLabel id="demo-simple-select-helper-label">
-          Payment Mode
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          className="paymentModeSelect"
-          // value={age}
-          // onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <TextField
-          className="installmentPremiumInput"
-          label="Installment Premium"
-          type="number"
-        />
-        <TextField className="gstInput" label="GST" type="number" />
-        <TextField
-          className="totalPremiumOutput"
-          label="Total Premium"
-          type="number"
-          value="30"
-          disabled
-        />
-        <TextField className="fileInput" label="Policy Copy" type="file" />
-        <Button className="saveLifePolicyBtn" color="primary" label="Save">
-          Save
-        </Button>
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField
+            className={classes.mobileNumber}
+            label="Mobile"
+            type="number"
+          />
+        </div>
+        <div className={classes.elementDiv}>
+          <MuiPickersUtilsProvider utils={Moment}>
+            <KeyboardDatePicker
+              margin="normal"
+              className={classes.datePicker}
+              id="date-picker-dialog"
+              label="Date of Commencement"
+              format="MM/dd/yyyy"
+              //value={selectedDate}
+              //onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date"
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField label="Plan/Term" className={classes.planInput} />
+        </div>
+        <div className={classes.elementDiv}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Payment Mode
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              // value={age}
+              // onChange={handleChange}
+              // labelWidth={labelWidth}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField
+            className={classes.installmentPremiumInput}
+            label="Installment Premium"
+            type="number"
+          />
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField className={classes.gstInput} label="GST" type="number" />
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField
+            className={classes.totalPremiumOutput}
+            label="Total Premium"
+            type="number"
+            value="30"
+            disabled
+          />
+        </div>
+        <div className={classes.elementDiv}>
+          <TextField className="fileInput" label="Policy Copy" type="file" />
+        </div>
+        <div style={{ "text-align": "right" }}>
+          <Button
+            className={classes.saveLifePolicyBtn}
+            color="primary"
+            label="Save"
+          >
+            Save
+          </Button>
+        </div>
       </Paper>
     </>
   );
--- src/views/LifePolicy/lifePolicyStyles.js
+++ src/views/LifePolicy/lifePolicyStyles.js
@@ -1,3 +1,43 @@
 const lifePolicyStyles = {
-
+  outerPaper: {
+    marginTop: "3vh",
+    backgroundImage: "url(`../../assets/img/background.jpg`)"
+  },
+  elementDiv: {
+    textAlign: "center"
+  },
+  nameInput: {
+    minWidth: "40vh"
+  },
+  datePicker: {
+    minWidth: "40vh"
+  },
+  planInput: {
+    minWidth: "40vh",
+    width: "40vh"
+  },
+  formControl: {
+    marginTop: "2vh",
+    minWidth: "40vh"
+  },
+  installmentPremiumInput: {
+    minWidth: "40vh"
+  },
+  gstInput: {
+    minWidth: "40vh"
+  },
+  totalPremiumOutput: {
+    marginTop: "2vh",
+    minWidth: "40vh"
+  },
+  mobileNumber: {
+    minWidth: "40vh"
+  },
+  saveLifePolicyBtn: {
+    marginTop: "2vh",
+    marginRight: "10vh",
+    minWidth: "15vh"
+  }
 };
+
+export default lifePolicyStyles;
